334194b54bc6b2437a88f7c4ddb5b458754ad203
Pavlo Hurhu <pavlo.gurgu@gmail.com>
2024-04-22 20:35:29 +0300

    added-post-to-supabase

--- diplomap/app/settings/page.tsx
+++ diplomap/app/settings/page.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
+import React from "react";
 import { UserButton } from "@clerk/nextjs";
 import { auth, currentUser } from "@clerk/nextjs/server";
-export default async function Settings() {
 
-    const { userId } = auth();
- 
+export default async function Settings() {
+  const { userId } = auth();
+
   if (userId) {
-    // Query DB for user specific information or display assets only to signed in users 
+    // Query DB for user specific information or display assets only to signed in users
   }
- 
+
   // Get the Backend API User object when you need access to the user's information
-  const user = await currentUser()
-  
-  console.log('user', {user})
+  const user = await currentUser();
+
+  console.log("user", user);
   return (
     <>
       <span className="font-bold text-4xl">Settings</span>
-  <UserButton/>
-  <h2>Hello, {user?.firstName}!</h2>
-      <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
-      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
-    </>
+      <UserButton />
+      <h2>Hello, {user?.firstName}!</h2>
+      </>
   );
-};
-
+}
