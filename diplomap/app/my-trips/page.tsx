6a352e8c6c1343bd4bb6e486818e8917fdc19cd1
Pavlo Hurhu <pavlo.gurgu@gmail.com>
2024-04-24 13:15:10 +0300

    supabase-usertable-working

--- diplomap/app/my-trips/page.tsx
+++ diplomap/app/my-trips/page.tsx
@@ -23,6 +23,8 @@ const MyTrips = () => {
   const { user } = useUser();
   const userId = user?.id;
   const [trips, setTrips] = useState<Trip[]>([]);
+  const [otherTrips, setOtherTrips] = useState<Trip[]>([]);
+
 
   useEffect(() => {
     const fetchTrips = async () => {
@@ -44,9 +46,31 @@ const MyTrips = () => {
     if (userId) {
       fetchTrips();
     }
+    // const fetchOtherTrips = async () => {
+    //   try {
+    //     // Запит до Supabase для отримання списку подорожей для певного user_id
+    //     const { data, error } = await supabase
+    //       .from("diplomap")
+    //       .select("*")
+    //       .eq("user_id", userId)
+    //       .eq("private", false);
+    //     if (error) {
+    //       throw error;
+    //     }
+    //     setOtherTrips(data);
+    //   } catch (error) {
+    //     console.error("Error fetching trips:", error);
+    //   }
+    // };
+    
+    // if (userId) {
+    //   fetchOtherTrips();
+    // }
+    
   }, [userId]);
 
   return (
+    <>
     <div>
       <h2>Trips for User {userId}</h2>
       <ol>
@@ -60,8 +84,24 @@ const MyTrips = () => {
             id {trip.trip_id} {trip.origin} - {trip.destination}
           </Link></li>
         ))}
-      </ol>
+      </ol> 
     </div>
+     <div>
+     <h3>Other Trips</h3>
+     <ol>
+       {/* {trip.id}{trip.destination}{trip.origin}{trip.distance}{trip.duration}{trip.transport_type}{trip.trip_id}{trip.user_id} */}
+       {otherTrips.map((trip) => (
+         // <li key={trip.id}><Link href="/my-trips/edit-trip" key = {trip.trip_id}>id {trip.trip_id} {trip.origin} - {trip.destination}</Link></li>
+         <li key={trip.id}> <Link
+           href={`/my-trips/edit-trip?trip_id=${trip.trip_id}`}
+           key={trip.trip_id}
+         >
+           id {trip.trip_id} {trip.origin} - {trip.destination} UID{trip.user_id}
+         </Link></li>
+       ))}
+     </ol>
+   </div>
+   </>
   );
 };
 
--- diplomap/app/settings/page.tsx
+++ diplomap/app/settings/page.tsx
@@ -4,15 +4,12 @@ import { auth, currentUser } from "@clerk/nextjs/server";
 
 export default async function Settings() {
   const { userId } = auth();
-
   if (userId) {
     // Query DB for user specific information or display assets only to signed in users
   }
-
   // Get the Backend API User object when you need access to the user's information
   const user = await currentUser();
 
-  console.log("user", user);
   return (
     <>
       <span className="font-bold text-4xl">Settings</span>
